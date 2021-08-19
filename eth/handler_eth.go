677fdb261e4f2e70b6a56a86232c9629403bcc17
Winston Smith <int20@pm.me>
2021-08-19 20:04:53 +0800

    replace ID() with full Enode

--- eth/handler_eth.go
+++ eth/handler_eth.go
@@ -63,7 +63,7 @@ func (h *ethHandler) AcceptTxs() bool {
 // message that the handler couldn't consume and serve itself.
 func (h *ethHandler) Handle(peer *eth.Peer, packet eth.Packet) error {
 	// Consume any broadcasts and announces, forwarding the rest to the downloader
-	id := peer.ID() + "@" + peer.RemoteAddr().String()
+	id := peer.Info().Enode
 	switch packet := packet.(type) {
 	case *eth.BlockHeadersPacket:
 		return h.handleHeaders(peer, *packet)
