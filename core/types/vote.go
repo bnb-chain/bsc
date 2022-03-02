0735293bffa34a63f396d89e25d4d81c86baa72a
Roshan <luoshen1997@gmail.com>
2022-03-02 10:19:46 +0800

    Some updates

--- core/types/vote.go
+++ core/types/vote.go
@@ -1,35 +1,35 @@
 package types
 
 import (
+	"sync/atomic"
+
 	"github.com/ethereum/go-ethereum/common"
 )
 
 const (
 	BLSPublicKeyLength = 48
 	BLSSignatureLength = 96
-
-	MaxDiffForkDist = 11 // Maximum allowed backward distance from the chain head
 )
 
 type BLSPublicKey [BLSPublicKeyLength]byte
 type BLSSignature [BLSSignatureLength]byte
 type ValidatorsBitSet uint64
 
-// Bytes gets the string representation of the underlying BLS public key.
-func (p BLSPublicKey) Bytes() []byte { return p[:] }
-
 type VoteData struct {
 	BlockNumber uint64
 	BlockHash   common.Hash
 }
 
-type VoteRecord struct {
+type VoteEnvelope struct {
 	VoteAddress BLSPublicKey
 	Signature   BLSSignature
 	Data        VoteData
+
+	// caches
+	hash atomic.Value
 }
 
-type AggVoteRecord struct {
+type AggVoteEnvelope struct {
 	VoteAddressSet ValidatorsBitSet
 	AggSignature   BLSSignature
 	Data           VoteData
--- params/protocol_params.go
+++ params/protocol_params.go
@@ -124,6 +124,7 @@ const (
 	Ripemd160PerWordGas uint64 = 120  // Per-word price for a RIPEMD160 operation
 	IdentityBaseGas     uint64 = 15   // Base price for a data copy operation
 	IdentityPerWordGas  uint64 = 3    // Per-work price for a data copy operation
+	SignatureVerifyGas  uint64 = 300  // Finality signature verify gas price
 
 	Bn256AddGasByzantium             uint64 = 500    // Byzantium gas needed for an elliptic curve addition
 	Bn256AddGasIstanbul              uint64 = 150    // Gas needed for an elliptic curve addition
