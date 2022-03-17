55349e00373cd4e1f450fb58b648f8798bdf550f
flywukong <2229306838@qq.com>
2022-03-17 12:00:21 +0800

    statedb copy use CopyWithSharedStorage

--- core/state/shared_pool.go
+++ core/state/shared_pool.go
@@ -7,29 +7,29 @@ import (
 
 // sharedStorage is used to store maps of originStorage of stateObjects
 type SharedStorage struct {
-	poolLock   *sync.RWMutex
-	shared_map map[common.Address]*sync.Map
+	*sync.RWMutex
+	sharedMap map[common.Address]*sync.Map
 }
 
 func NewSharedStorage() *SharedStorage {
 	sharedMap := make(map[common.Address]*sync.Map, 1500)
 	return &SharedStorage{
-		poolLock:   &sync.RWMutex{},
-		shared_map: sharedMap,
+		&sync.RWMutex{},
+		sharedMap,
 	}
 }
 
 // Check whether the storage exist in pool,
 // new one if not exist, it will be fetched in stateObjects.GetCommittedState()
 func (storage *SharedStorage) getOrInertStorage(address common.Address) *sync.Map {
-	storage.poolLock.RLock()
-	storageMap, ok := storage.shared_map[address]
-	storage.poolLock.RUnlock()
+	storage.RLock()
+	storageMap, ok := storage.sharedMap[address]
+	storage.RUnlock()
 	if !ok {
 		m := new(sync.Map)
-		storage.poolLock.Lock()
-		storage.shared_map[address] = m
-		storage.poolLock.Unlock()
+		storage.Lock()
+		storage.sharedMap[address] = m
+		storage.Unlock()
 		return m
 	}
 	return storageMap
