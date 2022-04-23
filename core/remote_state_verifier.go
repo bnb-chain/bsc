315075564f5264c58aa3fc4383092039d080119d
j75689 <j75689@gmail.com>
2022-04-23 16:29:41 +0800

    fix log format

--- core/remote_state_verifier.go
+++ core/remote_state_verifier.go
@@ -301,7 +301,7 @@ func (vt *verifyTask) Start(verifyCh chan common.Hash) {
 				vt.badPeers[msg.peerId] = struct{}{}
 				log.Info("peer is not available", "hash", msg.verifyResult.BlockHash, "number", msg.verifyResult.BlockNumber, "peer", msg.peerId, "reason", msg.verifyResult.Status.Msg)
 			case types.StatusBlockTooNew, types.StatusBlockNewer, types.StatusPossibleFork:
-				log.Info("return msg from peer %s for block %s is %s", msg.peerId, msg.verifyResult.BlockHash, msg.verifyResult.Status.Msg)
+				log.Info("return msg from peer", "peerId", msg.peerId, "hash", msg.verifyResult.BlockHash, "msg", msg.verifyResult.Status.Msg)
 			}
 			newVerifyMsgTypeGauge(msg.verifyResult.Status.Code, msg.peerId).Inc(1)
 		case <-resend.C:
