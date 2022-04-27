5a254c48e33d9d4996af642e24b66db75c2c531e
j75689 <j75689@gmail.com>
2022-04-27 10:20:27 +0800

    add interval when getting ErrSnapshotStale

--- core/state/state_object.go
+++ core/state/state_object.go
@@ -278,6 +278,14 @@ func (s *StateObject) GetCommittedState(db Database, key common.Hash) common.Has
 	}
 	// ErrSnapshotStale may occur due to diff layers in the update, so we should try again in noTrie mode.
 	if s.db.NoTrie() && err != nil && errors.Is(err, snapshot.ErrSnapshotStale) {
+		// This error occurs when statedb.snaps.Cap changes the state of the merged difflayer
+		// to stale during the refresh of the difflayer, indicating that it is about to be discarded.
+		// Since the difflayer is refreshed in parallel,
+		// there is a small chance that the difflayer of the stale will be read while reading,
+		// resulting in an empty array being returned here.
+		// Therefore, noTrie mode must retry here,
+		// and add a time interval when retrying to avoid stacking too much and causing OOM.
+		time.Sleep(snapshotStaleRetryInterval)
 		return s.GetCommittedState(db, key)
 	}
 	// If snapshot unavailable or reading from it failed, load from the database
--- core/state/statedb.go
+++ core/state/statedb.go
@@ -39,7 +39,10 @@ import (
 	"github.com/ethereum/go-ethereum/trie"
 )
 
-const defaultNumOfSlots = 100
+const (
+	defaultNumOfSlots          = 100
+	snapshotStaleRetryInterval = time.Millisecond * 100
+)
 
 type revision struct {
 	id           int
@@ -677,6 +680,14 @@ func (s *StateDB) getDeletedStateObject(addr common.Address) *StateObject {
 
 	// ErrSnapshotStale may occur due to diff layers in the update, so we should try again in noTrie mode.
 	if s.NoTrie() && err != nil && errors.Is(err, snapshot.ErrSnapshotStale) {
+		// This error occurs when statedb.snaps.Cap changes the state of the merged difflayer
+		// to stale during the refresh of the difflayer, indicating that it is about to be discarded.
+		// Since the difflayer is refreshed in parallel,
+		// there is a small chance that the difflayer of the stale will be read while reading,
+		// resulting in an empty array being returned here.
+		// Therefore, noTrie mode must retry here,
+		// and add a time interval when retrying to avoid stacking too much and causing OOM.
+		time.Sleep(snapshotStaleRetryInterval)
 		return s.getDeletedStateObject(addr)
 	}
 
