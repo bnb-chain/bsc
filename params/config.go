2941c87c072dddeadddb92f6f3798e9e465b715a
Loverush <luoshen1997@gmail.com>
2022-05-06 15:19:21 +0800

    Update system contracts' bytecode

--- params/config.go
+++ params/config.go
@@ -34,7 +34,7 @@ var (
 
 	BSCGenesisHash    = common.HexToHash("0x0d21840abff46b96c84b2ac9e10e4f5cdaeb5693cb665db62a2f3b02d2d57b5b")
 	ChapelGenesisHash = common.HexToHash("0x6d3c66c5357ec91d5c43af47e234a939b22557cbb552dc45bebbceeed90fbe34")
-	RialtoGenesisHash = common.HexToHash("0x6b5cf721c57dec67700d664c98c4f779b7bb980301b9a649842ba584fd72dd6c")
+	RialtoGenesisHash = common.HexToHash("0x2827bb62fad12d601843f6bc8a9ac4cd4679af53b418d034d4efa24a99a85123")
 	YoloV3GenesisHash = common.HexToHash("0xf1f2876e8500c77afcc03228757b39477eceffccf645b734967fe3c7e16967b7")
 )
 
