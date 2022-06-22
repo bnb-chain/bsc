926a43124d78486828aa7bd11d43bc902f63ec3b
qinglin89 <316032931@qq.com>
2022-06-22 14:24:09 +0800

    enable trie_pretcher on the copied statedb for prefetcher

--- core/state_prefetcher.go
+++ core/state_prefetcher.go
@@ -58,7 +58,7 @@ func (p *statePrefetcher) Prefetch(block *types.Block, statedb *state.StateDB, c
 	// No need to execute the first batch, since the main processor will do it.
 	for i := 0; i < prefetchThread; i++ {
 		go func() {
-			newStatedb := statedb.Copy()
+			newStatedb := statedb.Cpy4Prefetcher()
 			newStatedb.EnableWriteOnSharedStorage()
 			gaspool := new(GasPool).AddGas(block.GasLimit())
 			blockContext := NewEVMBlockContext(header, p.bc, nil)
@@ -153,5 +153,10 @@ func precacheTransaction(msg types.Message, config *params.ChainConfig, gaspool 
 	// Update the evm with the new transaction context.
 	evm.Reset(NewEVMTxContext(msg), statedb)
 	// Add addresses to access list if applicable
-	ApplyMessage(evm, msg, gaspool)
+	if _, err := ApplyMessage(evm, msg, gaspool); err != nil {
+		return
+	}
+	if config.IsByzantium(header.Number) {
+		statedb.Finalise4Prefetcher(true)
+	}
 }
