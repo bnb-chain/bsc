3e047d492d709a52e122af88a4a756bc1aebb0a3
lightclient <14004106+lightclient@users.noreply.github.com>
2022-06-24 18:03:13 +0200

    internal/ethapi: always return chain id (#25166)
    The error was introduced in PR #21686, but there is no good reason to enforce sync
    in this method, and it causes issues with EL/CL integration.

--- internal/ethapi/api.go
+++ internal/ethapi/api.go
@@ -623,12 +623,8 @@ func NewPublicBlockChainAPI(b Backend) *PublicBlockChainAPI {
 }
 
 // ChainId is the EIP-155 replay-protection chain id for the current ethereum chain config.
-func (api *PublicBlockChainAPI) ChainId() (*hexutil.Big, error) {
-	// if current block is at or past the EIP-155 replay-protection fork block, return chainID from config
-	if config := api.b.ChainConfig(); config.IsEIP155(api.b.CurrentBlock().Number()) {
-		return (*hexutil.Big)(config.ChainID), nil
-	}
-	return nil, fmt.Errorf("chain not synced beyond EIP-155 replay-protection fork block")
+func (api *PublicBlockChainAPI) ChainId() *hexutil.Big {
+	return (*hexutil.Big)(api.b.ChainConfig().ChainID)
 }
 
 // BlockNumber returns the block number of the chain head.
