a2a90d321286b48f6f84e82d526503af9e0641d5
Leon <316032931@qq.com>
2022-07-27 01:24:28 +0800

    Fix pipecommit about activeState (#1002)
    * get copy of prefetcher before use to avoid been modified between access and not-nil condition

--- core/state/state_object.go
+++ core/state/state_object.go
@@ -169,10 +169,11 @@ func (s *StateObject) getTrie(db Database) Trie {
 	if s.trie == nil {
 		// Try fetching from prefetcher first
 		// We don't prefetch empty tries
-		if s.data.Root != emptyRoot && s.db.prefetcher != nil {
+		prefetcher := s.db.prefetcher
+		if s.data.Root != emptyRoot && prefetcher != nil {
 			// When the miner is creating the pending state, there is no
 			// prefetcher
-			s.trie = s.db.prefetcher.trie(s.data.Root)
+			s.trie = prefetcher.trie(s.data.Root)
 		}
 		if s.trie == nil {
 			var err error
@@ -375,8 +376,9 @@ func (s *StateObject) finalise(prefetch bool) {
 		}
 	}
 
-	if s.db.prefetcher != nil && prefetch && len(slotsToPrefetch) > 0 && s.data.Root != emptyRoot && s.data.Root != dummyRoot {
-		s.db.prefetcher.prefetch(s.data.Root, slotsToPrefetch, s.addrHash)
+	prefetcher := s.db.prefetcher
+	if prefetcher != nil && prefetch && len(slotsToPrefetch) > 0 && s.data.Root != emptyRoot && s.data.Root != dummyRoot {
+		prefetcher.prefetch(s.data.Root, slotsToPrefetch, s.addrHash)
 	}
 	if len(s.dirtyStorage) > 0 {
 		s.dirtyStorage = make(Storage)
@@ -450,9 +452,11 @@ func (s *StateObject) updateTrie(db Database) Trie {
 	}
 	wg.Wait()
 
-	if s.db.prefetcher != nil {
-		s.db.prefetcher.used(s.data.Root, usedStorage)
+	prefetcher := s.db.prefetcher
+	if prefetcher != nil {
+		prefetcher.used(s.data.Root, usedStorage)
 	}
+
 	if len(s.pendingStorage) > 0 {
 		s.pendingStorage = make(Storage)
 	}
