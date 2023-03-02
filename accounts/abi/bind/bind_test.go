ae9847c1f60d5aaa1c7f5163c5cf4ce6fada956f
yutianwu <wzxingbupt@gmail.com>
2023-03-02 17:42:54 +0800

    fix: fix the ci tests

--- accounts/abi/bind/bind_test.go
+++ accounts/abi/bind/bind_test.go
@@ -2114,7 +2114,7 @@ func TestGolangBindings(t *testing.T) {
 	if out, err := replacer.CombinedOutput(); err != nil {
 		t.Fatalf("failed to replace tendermint dependency to bnb-chain source: %v\n%s", err, out)
 	}
-	tidier := exec.Command(gocmd, "mod", "tidy", "-compat=1.17")
+	tidier := exec.Command(gocmd, "mod", "tidy", "-compat=1.19")
 	tidier.Dir = pkg
 	if out, err := tidier.CombinedOutput(); err != nil {
 		t.Fatalf("failed to tidy Go module file: %v\n%s", err, out)
