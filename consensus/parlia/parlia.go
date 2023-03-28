daeebc8bb9e4c26db9bd378d880a89d2166a417b
qinglin89 <316032931@qq.com>
2023-03-28 15:15:56 +0800

    consensus/parlia: abort sealing when imported more than one block

--- consensus/parlia/parlia.go
+++ consensus/parlia/parlia.go
@@ -891,7 +891,7 @@ func (p *Parlia) Seal(chain consensus.ChainHeaderReader, block *types.Block, res
 				log.Info("Received block process finished, abort block seal")
 				return
 			case <-time.After(time.Duration(processBackOffTime) * time.Second):
-				if chain.CurrentHeader().Number.Uint64() == header.Number.Uint64() {
+				if number := header.Number.Uint64(); chain.CurrentHeader().Number.Uint64() >= number && chain.GetHeaderByNumber(number-1).Hash() == (header.ParentHash) {
 					log.Info("Process backoff time exhausted, and current header has updated to abort this seal")
 					return
 				}
