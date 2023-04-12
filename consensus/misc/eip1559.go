506c36c14d87bf76d07582af641c6b3176f886e9
Mister-EA <elton.antonis@binance.com>
2023-04-12 12:21:48 +0200

    consensus: remove commented out code

--- consensus/misc/eip1559.go
+++ consensus/misc/eip1559.go
@@ -60,37 +60,4 @@ func CalcBaseFee(config *params.ChainConfig, parent *types.Header) *big.Int {
 		return new(big.Int).SetUint64(params.InitialBaseFee)
 	}
 	return new(big.Int).SetUint64(params.InitialBaseFee)
-
-	// var (
-	// 	parentGasTarget          = parent.GasLimit / params.ElasticityMultiplier
-	// 	parentGasTargetBig       = new(big.Int).SetUint64(parentGasTarget)
-	// 	baseFeeChangeDenominator = new(big.Int).SetUint64(params.BaseFeeChangeDenominator)
-	// )
-	// // If the parent gasUsed is the same as the target, the baseFee remains unchanged.
-	// if parent.GasUsed == parentGasTarget {
-	// 	return new(big.Int).Set(parent.BaseFee)
-	// }
-	// if parent.GasUsed > parentGasTarget {
-	// 	// If the parent block used more gas than its target, the baseFee should increase.
-	// 	gasUsedDelta := new(big.Int).SetUint64(parent.GasUsed - parentGasTarget)
-	// 	x := new(big.Int).Mul(parent.BaseFee, gasUsedDelta)
-	// 	y := x.Div(x, parentGasTargetBig)
-	// 	baseFeeDelta := math.BigMax(
-	// 		x.Div(y, baseFeeChangeDenominator),
-	// 		common.Big1,
-	// 	)
-
-	// 	return x.Add(parent.BaseFee, baseFeeDelta)
-	// } else {
-	// 	// Otherwise if the parent block used less gas than its target, the baseFee should decrease.
-	// 	gasUsedDelta := new(big.Int).SetUint64(parentGasTarget - parent.GasUsed)
-	// 	x := new(big.Int).Mul(parent.BaseFee, gasUsedDelta)
-	// 	y := x.Div(x, parentGasTargetBig)
-	// 	baseFeeDelta := x.Div(y, baseFeeChangeDenominator)
-
-	// 	return math.BigMax(
-	// 		x.Sub(parent.BaseFee, baseFeeDelta),
-	// 		common.Big0,
-	// 	)
-	// }
 }
