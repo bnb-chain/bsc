83aa7dc0796f0f3fb415e3b853089a56c250866f
Sunny <sunny2022.za@gmail.com>
2023-04-18 10:04:19 +0800

    Revert "Implement 2 EIPS: limit and meter initcode and PUSH0 instructions (#1443)"
    This reverts commit b4f1cdffd55b7e27facec4d250009ad440d8ac74.
    
    Revert because previous hardfork (berlin, london) need to be merged
    first to avoid dependencies risk

--- core/vm/opcodes.go
+++ core/vm/opcodes.go
@@ -116,7 +116,6 @@ const (
 	MSIZE    OpCode = 0x59
 	GAS      OpCode = 0x5a
 	JUMPDEST OpCode = 0x5b
-	PUSH0    OpCode = 0x5f
 )
 
 // 0x60 range - pushes.
@@ -298,7 +297,6 @@ var opCodeToString = map[OpCode]string{
 	MSIZE:    "MSIZE",
 	GAS:      "GAS",
 	JUMPDEST: "JUMPDEST",
-	PUSH0:    "PUSH0",
 
 	// 0x60 range - push.
 	PUSH1:  "PUSH1",
@@ -462,7 +460,6 @@ var stringToOp = map[string]OpCode{
 	"MSIZE":          MSIZE,
 	"GAS":            GAS,
 	"JUMPDEST":       JUMPDEST,
-	"PUSH0":          PUSH0,
 	"PUSH1":          PUSH1,
 	"PUSH2":          PUSH2,
 	"PUSH3":          PUSH3,
