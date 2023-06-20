a27ece8d0a99afb40f0cd0a11c76029de730f6fd
KeefeL <90749943+KeefeL@users.noreply.github.com>
2023-06-20 18:04:18 +0800

    lightclient: fix validator set change (#1645)

--- core/vm/lightclient/v2/lightclient.go
+++ core/vm/lightclient/v2/lightclient.go
@@ -83,7 +83,7 @@ func (cs ConsensusState) EncodeConsensusState() ([]byte, error) {
 	return encodingBytes, nil
 }
 
-func (cs *ConsensusState) ApplyLightBlock(block *types.LightBlock) (bool, error) {
+func (cs *ConsensusState) ApplyLightBlock(block *types.LightBlock, isHertz bool) (bool, error) {
 	if uint64(block.Height) <= cs.Height {
 		return false, fmt.Errorf("block height <= consensus height (%d < %d)", block.Height, cs.Height)
 	}
@@ -118,12 +118,19 @@ func (cs *ConsensusState) ApplyLightBlock(block *types.LightBlock) (bool, error)
 		}
 	}
 
+	valSetChanged := !(bytes.Equal(cs.ValidatorSet.Hash(), block.ValidatorsHash))
+
 	// update consensus state
 	cs.Height = uint64(block.Height)
 	cs.NextValidatorSetHash = block.NextValidatorsHash
 	cs.ValidatorSet = block.ValidatorSet
 
-	return !(bytes.Equal(cs.ValidatorSet.Hash(), block.ValidatorsHash)), nil
+	if !isHertz {
+		// This logic is wrong, fixed in hertz fork.
+		return !(bytes.Equal(cs.ValidatorSet.Hash(), block.ValidatorsHash)), nil
+	}
+
+	return valSetChanged, nil
 }
 
 // input:
