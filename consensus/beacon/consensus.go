30aa918d04c7a7809f7d9ec0120d86a70b2d6d9b
ethereal-chain <139233306+ethereal-chain@users.noreply.github.com>
2023-07-11 17:54:36 +0530

    Update consensus.go

--- consensus/beacon/consensus.go
+++ consensus/beacon/consensus.go
@@ -6,7 +6,7 @@
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// The go-ethereum library is distributed in the hope that it will be useful,
+// The go-ether  eum library is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU Lesser General Public License for more details.
