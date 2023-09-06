cd73bef8891ecc55e09eb55d9c2e7db762d4e637
joeycli <joeycli0919@gmail.com>
2023-09-06 12:40:34 +0800

    chore: add debug log for snap dirty data

--- core/state/statedb.go
+++ core/state/statedb.go
@@ -190,7 +190,7 @@ func newStateDB(root common.Hash, db Database, snaps *snapshot.Tree) (*StateDB, 
 		db:                   db,
 		trie:                 tr,
 		originalRoot:         root,
-		snaps:                nil, // snaps data is dirty, will be fixed in furture
+		snaps:                snaps,
 		accounts:             make(map[common.Address][]byte),
 		storages:             make(map[common.Address]map[string][]byte),
 		accountsOrigin:       make(map[common.Address][]byte),
