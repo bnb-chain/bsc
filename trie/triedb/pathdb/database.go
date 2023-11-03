4f85e2dc112d93c462629d3d083490be29ba1cec
buddh0 <galaxystroller@gmail.com>
2023-11-03 17:17:41 +0800

    core: enable Shanghai EIPs

--- trie/triedb/pathdb/database.go
+++ trie/triedb/pathdb/database.go
@@ -172,7 +172,7 @@ func New(diskdb ethdb.Database, config *Config) *Database {
 	// mechanism also ensures that at most one **non-readOnly** database
 	// is opened at the same time to prevent accidental mutation.
 	if ancient, err := diskdb.AncientDatadir(); err == nil && ancient != "" && !db.readOnly {
-		offset := uint64(0) //TODO(Nathan): just for passing compilation
+		offset := uint64(0) // differ from in block data, only metadata is used in state data
 		freezer, err := rawdb.NewStateFreezer(ancient, false, offset)
 		if err != nil {
 			log.Crit("Failed to open state history freezer", "err", err)
