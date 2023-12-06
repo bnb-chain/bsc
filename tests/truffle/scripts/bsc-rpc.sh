1f6e047cc358f260c97a41709d59743092a0dc0c
buddh0 <galaxystroller@gmail.com>
2023-12-06 18:32:31 +0800

    tests/truffle: adapt changes in bsc-genesis-contracts

--- tests/truffle/scripts/bsc-rpc.sh
+++ tests/truffle/scripts/bsc-rpc.sh
@@ -13,4 +13,4 @@ done
 geth --config ${DATA_DIR}/config.toml --datadir ${DATA_DIR} --netrestrict ${CLUSTER_CIDR} \
     --verbosity ${VERBOSE} --nousb \
     --rpc.allow-unprotected-txs --history.transactions 15768000 \
-    -unlock ${unlock_sequences} --password /dev/null
+    -unlock ${unlock_sequences} --password /dev/null  >${DATA_DIR}/bscnode-rpc.log
