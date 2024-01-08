1b95221366283f5f6ea5bd7c6bf5f89e01bd5593
Roshan <luoshen1997@gmail.com>
2024-01-08 11:02:41 +0800

    chore: update abi and bytecode

--- consensus/parlia/feynmanfork.go
+++ consensus/parlia/feynmanfork.go
@@ -18,11 +18,12 @@ import (
 	"github.com/ethereum/go-ethereum/rpc"
 )
 
-// TODO: revert to normal value
+// TODO: SecondsPerDay represents the seconds in a day, it should be 86400
+// We set it to 60 for testing purpose and we will change it back to 86400 when launching
 // const SecondsPerDay uint64 = 86400
 const SecondsPerDay uint64 = 60
 
-// the parmas should be blocks' time which are timestamp
+// the params should be two blocks' time(timestamp)
 func sameDayInUTC(first, second uint64) bool {
 	return first/SecondsPerDay == second/SecondsPerDay
 }
