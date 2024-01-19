d356a6dbe2f67cb5ce7b84fcd65926b3e7e01877
Roshan <luoshen1997@gmail.com>
2024-01-19 14:04:24 +0800

    fix review comments

--- core/chain_makers.go
+++ core/chain_makers.go
@@ -26,12 +26,12 @@ import (
 	"github.com/ethereum/go-ethereum/consensus/misc/eip1559"
 	"github.com/ethereum/go-ethereum/core/rawdb"
 	"github.com/ethereum/go-ethereum/core/state"
+	"github.com/ethereum/go-ethereum/core/systemcontracts"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/core/vm"
 	"github.com/ethereum/go-ethereum/ethdb"
 	"github.com/ethereum/go-ethereum/params"
 	"github.com/ethereum/go-ethereum/trie"
-	"github.com/ethereum/go-ethereum/core/systemcontracts"
 )
 
 // BlockGen creates blocks for testing.
