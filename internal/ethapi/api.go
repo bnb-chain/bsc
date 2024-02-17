c58fe9cac2be7e53c2d2af1203030410e9345115
Виталий Кисель <kisel@internet.ru>
2024-02-17 16:49:11 +0300

    up

--- internal/ethapi/api.go
+++ internal/ethapi/api.go
@@ -1840,6 +1840,9 @@ func AccessList(ctx context.Context, b Backend, db *state.StateDB, header *types
 	if err := args.setDefaults(ctx, b); err != nil {
 		return nil, 0, nil, err
 	}
+
+	fmt.Printf("after setDefaults\n")
+	
 	var to common.Address
 	if args.To != nil {
 		to = *args.To
