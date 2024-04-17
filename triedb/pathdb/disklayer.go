925fcc388d22c9619e8d56b368f484b92353b084
Fynn <zcheng1004@gmail.com>
2024-04-17 15:24:18 +0800

    pathdb: add nodes from readdirectly to nodes cache

--- triedb/pathdb/disklayer.go
+++ triedb/pathdb/disklayer.go
@@ -310,6 +310,7 @@ func (dl *diskLayer) readAccountTrie(hash common.Hash) []byte {
 	if nBlob == nil {
 		return nil
 	}
+	dl.cleans.nodes.Set(cacheKey(common.Hash{}, path[:]), nBlob)
 	val, key := trie.DecodeLeafNode(nHash.Bytes(), path, nBlob)
 	if bytes.Compare(key, hash.Bytes()) == 0 {
 		return val
@@ -331,6 +332,7 @@ func (dl *diskLayer) readStorageTrie(accountHash, storageHash common.Hash) []byt
 	if nBlob == nil {
 		return nil
 	}
+	dl.cleans.nodes.Set(cacheKey(accountHash, path[common.HashLength:]), nBlob)
 
 	val, key := trie.DecodeLeafNode(nHash.Bytes(), path[common.HashLength:], nBlob)
 	if bytes.Compare(storageHash.Bytes(), key) == 0 {
