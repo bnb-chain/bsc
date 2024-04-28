b33e1d66ede367359131502b13f8fae55bb6ac02
Roshan <48975233+Pythonberg1997@users.noreply.github.com>
2024-04-28 13:33:59 +0800

    fix: typo in `BundlePool.AllBundles` (#24)

--- core/txpool/bundlepool/bundlepool.go
+++ core/txpool/bundlepool/bundlepool.go
@@ -191,7 +191,7 @@ func (p *BundlePool) PendingBundles(blockNumber uint64, blockTimestamp uint64) [
 
 // AllBundles returns all the bundles currently in the pool
 func (p *BundlePool) AllBundles() []*types.Bundle {
-	p.mu.RUnlock()
+	p.mu.RLock()
 	defer p.mu.RUnlock()
 	bundles := make([]*types.Bundle, 0, len(p.bundles))
 	for _, bundle := range p.bundles {
