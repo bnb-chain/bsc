2331c09c188fa961d0832ff7d9f7548ec830f22b
qybdyx <qybdyx@outlook.com>
2024-05-14 18:02:26 +0800

    eth/fetcher/block_fetcher.go: fix go lint

--- eth/fetcher/block_fetcher.go
+++ eth/fetcher/block_fetcher.go
@@ -19,10 +19,10 @@ package fetcher
 
 import (
 	"errors"
-	mapset "github.com/deckarep/golang-set/v2"
 	"math/rand"
 	"time"
 
+	mapset "github.com/deckarep/golang-set/v2"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/prque"
 	"github.com/ethereum/go-ethereum/consensus"
