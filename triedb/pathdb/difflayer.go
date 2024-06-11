5d5b309405afe1e0992976203310113e6b03b4ff
will@2012 <xibaow2020@qq.com>
2024-06-11 14:36:21 +0800

    chore: refine log/metrics

--- triedb/pathdb/difflayer.go
+++ triedb/pathdb/difflayer.go
@@ -266,13 +266,14 @@ func (dl *diffLayer) Node(owner common.Hash, path []byte, hash common.Hash) ([]b
 			// so in extreme cases, both reading the difflayer cache and reading the disklayer may fail.
 			// In this case, fallback to the original 128-layer recursive difflayer query path.
 			diffHashCacheSlowPathMeter.Mark(1)
-			log.Warn("Hash map and disklayer mismatch, retry difflayer", "owner", owner, "path", path, "hash", hash.String())
+			log.Debug("Hash map and disklayer mismatch, retry difflayer", "owner", owner, "path", path, "hash", hash.String())
 			return dl.node(owner, path, hash, 0)
 		} else {
 			// skip difflayer query, which is fastpath.
 			return blob, nil
 		}
 	}
+	diffHashCacheSlowPathMeter.Mark(1)
 	return dl.node(owner, path, hash, 0)
 }
 
