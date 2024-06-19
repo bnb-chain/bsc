a824dc9a0970d1f26ded05ea49a594ed992e8924
Jolly Zhao <zhaolei@pm.me>
2024-06-19 22:41:37 +0800

    fix: enhance GreedyMergeTx performance

--- miner/bid_simulator.go
+++ miner/bid_simulator.go
@@ -347,8 +347,6 @@ func (b *bidSimulator) newBidLoop() {
 				continue
 			}
 
-			bidRuntime := &BidRuntime{
-				bid:                     newBid,
 				expectedBlockReward:     expectedBlockReward,
 				expectedValidatorReward: expectedValidatorReward,
 				packedBlockReward:       big.NewInt(0),
@@ -637,7 +635,7 @@ func (b *bidSimulator) simBid(interruptCh chan int32, bidRuntime *BidRuntime) {
 	if b.config.GreedyMergeTx {
 		delay := b.engine.Delay(b.chain, bidRuntime.env.header, &b.delayLeftOver)
 		if delay != nil && *delay > 0 {
-			bidTxsSet := mapset.NewSet[common.Hash]()
+			bidTxsSet := mapset.NewThreadUnsafeSetWithSize[common.Hash](len(bidRuntime.bid.Txs))
 			for _, tx := range bidRuntime.bid.Txs {
 				bidTxsSet.Add(tx.Hash())
 			}
