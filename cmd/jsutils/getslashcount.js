2094e8c4e06e4c153e3a94001335453bf37c0280
qybdyx <qybdyx@outlook.com>
2024-07-08 21:12:59 +0800

    cmd/jsutils: add a tool to get slash count

---
+++ cmd/jsutils/getslashcount.js
@@ -0,0 +1,36 @@
+import { ethers } from "ethers";
+import program from "commander";
+
+program.option("--Rpc <Rpc>", "Rpc");
+program.option("--Num <Num>", "num")
+program.parse(process.argv);
+
+const provider = new ethers.JsonRpcProvider(program.Rpc);
+
+const slashAbi = [
+    "function getSlashIndicator(address validatorAddr) external view returns (uint256, uint256)"
+]
+const validatorSetAbi = [
+    "function getLivingValidators() external view returns (address[], bytes[])"
+]
+const addrValidatorSet = '0x0000000000000000000000000000000000001000';
+const addrSlash = '0x0000000000000000000000000000000000001001';
+const validatorSet = new ethers.Contract(addrValidatorSet, validatorSetAbi, provider);
+const slashIndicator = new ethers.Contract(addrSlash, slashAbi,  provider)
+
+
+const main = async () => {
+    console.log("current block", program.Num)
+    const blockNum = ethers.getBigInt(program.Num)
+    const data = await validatorSet.getLivingValidators({blockTag:blockNum})
+    for (let i = 0; i < data[0].length; i++) {
+        let addr = data[0][i];
+        let info = await slashIndicator.getSlashIndicator(addr, {blockTag:blockNum})
+        console.log(addr, ethers.toNumber(info[1]))
+    }
+};
+main().then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
