1217b01259fc021dd457ce5b4d506cdd3c2ee17e
buddh0 <galaxystroller@gmail.com>
2024-07-17 18:27:03 +0800

    consensus/parlia: add GetJustifiedNumber and GetFinalizedNumber

--- cmd/jsutils/get_perf.js
+++ cmd/jsutils/get_perf.js
@@ -12,6 +12,7 @@ const main = async () => {
     let txCountTotal = 0;
     let gasUsedTotal = 0;
     let inturnBlocks = 0;
+    let justifiedBlocks = 0;
     for (let i = program.startNum; i < program.endNum; i++) {
         let txCount = await provider.send("eth_getBlockTransactionCountByNumber", [
             ethers.toQuantity(i)]);
@@ -26,6 +27,14 @@ const main = async () => {
             inturnBlocks += 1
         }
         let timestamp = eval(eval(header.timestamp).toString(10))
+
+        let justifiedNumber = await provider.send("parlia_getJustifiedNumber", [
+            ethers.toQuantity(i)]);
+        if (justifiedNumber + 1 == i) {
+            justifiedBlocks += 1
+        } else {
+            console.log("justified unexpected", "BlockNumber =", i,"justifiedNumber",justifiedNumber)    
+        }
         console.log("BlockNumber =", i, "mod =", i%4, "miner =", header.miner , "difficulty =", difficulty, "txCount =", ethers.toNumber(txCount), "gasUsed", gasUsed, "timestamp", timestamp)
     }
 
@@ -41,13 +50,14 @@ const main = async () => {
     let timeCost = endTime - startTime
     let avgBlockTime = timeCost/blockCount
     let inturnBlocksRatio = inturnBlocks/blockCount
+    let justifiedBlocksRatio = justifiedBlocks/blockCount
     let tps = txCountTotal/timeCost
     let M = 1000000
     let avgGasUsedPerBlock = gasUsedTotal/blockCount/M
     let avgGasUsedPerSecond = gasUsedTotal/timeCost/M
 
     console.log("Get the performance between [", program.startNum, ",", program.endNum, ")");
-    console.log("txCountPerBlock =", txCountPerBlock, "txCountTotal =", txCountTotal, "BlockCount =", blockCount, "avgBlockTime =", avgBlockTime, "inturnBlocksRatio =", inturnBlocksRatio);
+    console.log("txCountPerBlock =", txCountPerBlock, "txCountTotal =", txCountTotal, "BlockCount =", blockCount, "avgBlockTime =", avgBlockTime, "inturnBlocksRatio =", inturnBlocksRatio, "justifiedBlocksRatio =", justifiedBlocksRatio);
     console.log("txCountPerSecond =", tps, "avgGasUsedPerBlock =", avgGasUsedPerBlock, "avgGasUsedPerSecond =", avgGasUsedPerSecond);
 };
 
