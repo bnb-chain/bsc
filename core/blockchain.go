1eb87068c80d32d1cc7d1c85ed403df01a03e21a
buddh0 <galaxystroller@gmail.com>
2024-08-06 13:47:54 +0800

    core: fix too many logs about new potential chain

--- core/blockchain.go
+++ core/blockchain.go
@@ -2340,7 +2340,7 @@ func (bc *BlockChain) insertChain(chain types.Blocks, setHead bool) (int, error)
 			snapDiffItems, snapBufItems, _ = bc.snaps.Size()
 		}
 		trieDiffNodes, trieBufNodes, trieImmutableBufNodes, _ := bc.triedb.Size()
-		stats.report(chain, it.index, snapDiffItems, snapBufItems, trieDiffNodes, trieBufNodes, trieImmutableBufNodes, status == CanonStatTy)
+		stats.report(chain, it.index, snapDiffItems, snapBufItems, trieDiffNodes, trieBufNodes, trieImmutableBufNodes, setHead && (status == CanonStatTy))
 
 		if !setHead {
 			// After merge we expect few side chains. Simply count
