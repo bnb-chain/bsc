fbf4d2a4c4b2e7f6e71768bf838e6ebefe90086f
tiaoxizhan <tiaoxizhan@outlook.com>
2024-09-13 18:17:42 +0800

    chore: add missing symbols in comment
    Signed-off-by: tiaoxizhan <tiaoxizhan@outlook.com>

--- crypto/bn256/cloudflare/gfp12.go
+++ crypto/bn256/cloudflare/gfp12.go
@@ -1,7 +1,7 @@
 package bn256
 
 // For details of the algorithms used, see "Multiplication and Squaring on
-// Pairing-Friendly Fields, Devegili et al.
+// Pairing-Friendly Fields", Devegili et al.
 // http://eprint.iacr.org/2006/471.pdf.
 
 import (
