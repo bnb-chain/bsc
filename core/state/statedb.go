6d41ff819085948a91f393961fe6b9b3ea2896bb
joeylichang <joeycli0919@gmail.com>
2024-09-30 16:49:06 +0800

    chore: delete debug info

--- core/state/statedb.go
+++ core/state/statedb.go
@@ -23,7 +23,6 @@ import (
 	"runtime"
 	"sort"
 	"sync"
-	"sync/atomic"
 	"time"
 
 	"github.com/ethereum/go-ethereum/common"
@@ -1217,7 +1216,6 @@ func (s *StateDB) AccountsIntermediateRoot() {
 		}()
 	}
 
-	var calcStateObjectCount atomic.Int64
 	// Although naively it makes sense to retrieve the account trie and then do
 	// the contract storage and account updates sequentially, that short circuits
 	// the account prefetcher. Instead, let's process all the storage updates
@@ -1228,9 +1226,6 @@ func (s *StateDB) AccountsIntermediateRoot() {
 			wg.Add(1)
 			tasks <- func() {
 				obj.updateRoot()
-				if obj.trie != nil {
-					calcStateObjectCount.Add(1)
-				}
 				// Cache the data until commit. Note, this update mechanism is not symmetric
 				// to the deletion, because whereas it is enough to track account updates
 				// at commit time, deletions need tracking at transaction boundary level to
@@ -1244,7 +1239,6 @@ func (s *StateDB) AccountsIntermediateRoot() {
 		}
 	}
 	wg.Wait()
-	log.Info("versa calc root state object", "count", calcStateObjectCount.Load(), "version", s.db.GetVersion()+1)
 }
 
 func (s *StateDB) StateIntermediateRoot() common.Hash {
@@ -1625,7 +1619,6 @@ func (s *StateDB) Commit(block uint64, failPostCommitFunc func(), postCommitFunc
 				}()
 			}
 
-			var committedStateObjectNum atomic.Int64
 			for addr := range s.stateObjectsDirty {
 				if obj := s.stateObjects[addr]; !obj.deleted {
 					tasks <- func() {
@@ -1635,9 +1628,6 @@ func (s *StateDB) Commit(block uint64, failPostCommitFunc func(), postCommitFunc
 								taskResults <- taskResult{err, nil}
 								return
 							} else {
-								if obj.trie != nil {
-									committedStateObjectNum.Add(1)
-								}
 								taskResults <- taskResult{nil, set}
 							}
 						} else {
@@ -1720,7 +1710,6 @@ func (s *StateDB) Commit(block uint64, failPostCommitFunc func(), postCommitFunc
 				}
 			}
 			wg.Wait()
-			log.Info("versa commit state object", "count", committedStateObjectNum.Load(), "version", s.db.GetVersion()+1)
 			return nil
 		}()
 
