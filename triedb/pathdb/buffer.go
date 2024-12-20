b7a83b4166add4a27e72ac6e99f3a211b0a5999d
galaio <galaio@users.noreply.github.com>
2024-12-20 20:14:30 +0800

    pbss: support to load legacy async buffer format;

--- triedb/pathdb/buffer.go
+++ triedb/pathdb/buffer.go
@@ -95,7 +95,12 @@ func (b *buffer) revertTo(db ethdb.KeyValueReader, nodes map[common.Hash]map[str
 		return nil
 	}
 	b.nodes.revertTo(db, nodes)
-	b.states.revertTo(accounts, storages)
+	// TODO(galaio): In order to be compatible with the legacy version,
+	// a temporary empty check is added, which may affect the reading
+	// result of pbss as flatReader, see: flatReader.Account()
+	if len(b.states.accountData) != 0 || len(b.states.storageData) != 0 {
+		b.states.revertTo(accounts, storages)
+	}
 	return nil
 }
 
