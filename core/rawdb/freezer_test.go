4b70db9895e668218a63d00f81c2f077e8fa582e
galaio <galaio@users.noreply.github.com>
2024-12-26 16:38:23 +0800

    freezer: remove prunedfreezer;

--- core/rawdb/freezer_test.go
+++ core/rawdb/freezer_test.go
@@ -112,7 +112,7 @@ func TestFreezerModifyRollback(t *testing.T) {
 
 	// Reopen and check that the rolled-back data doesn't reappear.
 	tables := map[string]bool{"test": true}
-	f2, err := NewFreezer(dir, "", false, 0, 2049, tables)
+	f2, err := NewFreezer(dir, "", false, 2049, tables)
 	if err != nil {
 		t.Fatalf("can't reopen freezer after failed ModifyAncients: %v", err)
 	}
@@ -253,7 +253,7 @@ func TestFreezerReadonlyValidate(t *testing.T) {
 	dir := t.TempDir()
 	// Open non-readonly freezer and fill individual tables
 	// with different amount of data.
-	f, err := NewFreezer(dir, "", false, 0, 2049, tables)
+	f, err := NewFreezer(dir, "", false, 2049, tables)
 	if err != nil {
 		t.Fatal("can't open freezer", err)
 	}
@@ -276,7 +276,7 @@ func TestFreezerReadonlyValidate(t *testing.T) {
 
 	// Re-opening as readonly should fail when validating
 	// table lengths.
-	_, err = NewFreezer(dir, "", true, 0, 2049, tables)
+	_, err = NewFreezer(dir, "", true, 2049, tables)
 	if err == nil {
 		t.Fatal("readonly freezer should fail with differing table lengths")
 	}
@@ -288,7 +288,7 @@ func TestFreezerConcurrentReadonly(t *testing.T) {
 	tables := map[string]bool{"a": true}
 	dir := t.TempDir()
 
-	f, err := NewFreezer(dir, "", false, 0, 2049, tables)
+	f, err := NewFreezer(dir, "", false, 2049, tables)
 	if err != nil {
 		t.Fatal("can't open freezer", err)
 	}
@@ -314,7 +314,7 @@ func TestFreezerConcurrentReadonly(t *testing.T) {
 		go func(i int) {
 			defer wg.Done()
 
-			f, err := NewFreezer(dir, "", true, 0, 2049, tables)
+			f, err := NewFreezer(dir, "", true, 2049, tables)
 			if err == nil {
 				fs[i] = f
 			} else {
@@ -337,7 +337,7 @@ func TestFreezer_AdditionTables(t *testing.T) {
 	dir := t.TempDir()
 	// Open non-readonly freezer and fill individual tables
 	// with different amount of data.
-	f, err := NewFreezer(dir, "", false, 0, 2049, map[string]bool{"o1": true, "o2": true})
+	f, err := NewFreezer(dir, "", false, 2049, map[string]bool{"o1": true, "o2": true})
 	if err != nil {
 		t.Fatal("can't open freezer", err)
 	}
@@ -363,11 +363,11 @@ func TestFreezer_AdditionTables(t *testing.T) {
 
 	// check read only
 	additionTables = []string{"a1"}
-	f, err = NewFreezer(dir, "", true, 0, 2049, map[string]bool{"o1": true, "o2": true, "a1": true})
+	f, err = NewFreezer(dir, "", true, 2049, map[string]bool{"o1": true, "o2": true, "a1": true})
 	require.NoError(t, err)
 	require.NoError(t, f.Close())
 
-	f, err = NewFreezer(dir, "", false, 0, 2049, map[string]bool{"o1": true, "o2": true, "a1": true})
+	f, err = NewFreezer(dir, "", false, 2049, map[string]bool{"o1": true, "o2": true, "a1": true})
 	require.NoError(t, err)
 	frozen, _ := f.Ancients()
 	require.NoError(t, f.ResetTable("a1", frozen, true))
@@ -410,7 +410,7 @@ func TestFreezer_AdditionTables(t *testing.T) {
 	require.NoError(t, f.Close())
 
 	// reopen and read
-	f, err = NewFreezer(dir, "", true, 0, 2049, map[string]bool{"o1": true, "o2": true, "a1": true})
+	f, err = NewFreezer(dir, "", true, 2049, map[string]bool{"o1": true, "o2": true, "a1": true})
 	require.NoError(t, err)
 
 	// recheck additional table boundary
@@ -440,7 +440,7 @@ func newFreezerForTesting(t *testing.T, tables map[string]bool) (*Freezer, strin
 	dir := t.TempDir()
 	// note: using low max table size here to ensure the tests actually
 	// switch between multiple files.
-	f, err := NewFreezer(dir, "", false, 0, 2049, tables)
+	f, err := NewFreezer(dir, "", false, 2049, tables)
 	if err != nil {
 		t.Fatal("can't open freezer", err)
 	}
