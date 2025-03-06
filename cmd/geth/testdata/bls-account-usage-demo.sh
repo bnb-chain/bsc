9ec5c87f07aac204155d90376a6041030fea3bae
buddh0 <galaxystroller@gmail.com>
2025-03-06 11:31:14 +0800

    cmd/geth: add example for `geth bls account generate-proof`

--- cmd/geth/testdata/bls-account-usage-demo.sh
+++ cmd/geth/testdata/bls-account-usage-demo.sh
@@ -23,10 +23,13 @@ ${workspace}/build/bin/geth bls account list  --blspassword ./bls-password.txt -
 
 echo "4. import a bls account by passing a keystore file------------------------------------------"
 keystoreFile=`ls bls1/bls/keystore`
-${workspace}/build/bin/geth bls account import  --importedaccountpassword ./bls-password1.txt --blspassword ./bls-password.txt --datadir ./bls ./bls1/bls/keystore/${keystoreFile} 
-${workspace}/build/bin/geth bls account list  --blspassword ./bls-password.txt --datadir ./bls
+${workspace}/build/bin/geth bls account import  --importedaccountpassword ./bls-password1.txt --blspassword ./bls-password.txt --datadir ./bls ./bls1/bls/keystore/${keystoreFile}
+publicKey=`${workspace}/build/bin/geth bls account list  --blspassword ./bls-password.txt --datadir ./bls |grep public | tail -1 | awk '{print $NF}'` 
 
-echo "5. clearup----------------------------------------------------------------------------------"
+echo "5. generate ownership proof for the selected BLS account from the BLS wallet----------------"
+${workspace}/build/bin/geth bls account generate-proof --blspassword ./bls-password.txt --datadir ./bls --chain-id 56 0x04d63aBCd2b9b1baa327f2Dda0f873F197ccd186 ${publicKey}
+
+echo "6. clearup----------------------------------------------------------------------------------"
 rm -rf bls
 rm -rf bls1
 rm -rf bls-password.txt
