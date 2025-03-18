d95bad1b3b4d466f11419f2345739a40ea405e45
Lee Bousfield <ljbousfield@gmail.com>
2025-03-18 09:41:34 -0500

    ethclient: Add EstimateGasAtBlock[Hash] to estimate against a specific block (#27508)
    The main use case I see of this is that it allows users to estimate gas
    against the same state that they query for their nonce, and the same
    state they base the data of their transaction against. This helps ensure
    that gas estimation won't fail and the transaction won't revert on-chain
    because of a mismatch between the state used for gas estimation and the
    state used to generate the inputs to gas estimation or the transaction's
    nonce when submitted to the mempool.
    
    This PR also updates the EstimateGas comment based on the new geth
    `eth_estimateGas` default of using latest state as of v1.12.0:
    https://github.com/ethereum/go-ethereum/pull/24363
    
    ---------
    
    Co-authored-by: Felix Lange <fjl@twurst.com>

--- ethclient/ethclient_test.go
+++ ethclient/ethclient_test.go
@@ -625,6 +625,33 @@ func testSendTransactionConditional(t *testing.T, client *rpc.Client) {
 	if err := sendTransactionConditional(ec); err != nil {
 		t.Fatalf("error: %v", err)
 	}
+	// Use HeaderByNumber to get a header for EstimateGasAtBlock and EstimateGasAtBlockHash
+	latestHeader, err := ec.HeaderByNumber(context.Background(), nil)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	// EstimateGasAtBlock
+	msg := ethereum.CallMsg{
+		From:  testAddr,
+		To:    &common.Address{},
+		Gas:   21000,
+		Value: big.NewInt(1),
+	}
+	gas, err := ec.EstimateGasAtBlock(context.Background(), msg, latestHeader.Number)
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	if gas != 21000 {
+		t.Fatalf("unexpected gas limit: %v", gas)
+	}
+	// EstimateGasAtBlockHash
+	gas, err = ec.EstimateGasAtBlockHash(context.Background(), msg, latestHeader.Hash())
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	if gas != 21000 {
+		t.Fatalf("unexpected gas limit: %v", gas)
+	}
 }
 
 func sendTransactionConditional(ec *ethclient.Client) error {
