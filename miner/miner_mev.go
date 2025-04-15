b14bbbd563eafa41e88459c962250ec1cf0bab7b
zzzckck <152148891+zzzckck@users.noreply.github.com>
2025-04-15 09:41:21 +0800

    mev: include MaxBidsPerBuilder in MevParams

--- miner/miner_mev.go
+++ miner/miner_mev.go
@@ -100,6 +100,7 @@ func (miner *Miner) MevParams() *types.MevParams {
 		ValidatorCommission:   *miner.worker.config.Mev.ValidatorCommission,
 		BidSimulationLeftOver: *miner.worker.config.Mev.BidSimulationLeftOver,
 		NoInterruptLeftOver:   *miner.worker.config.Mev.NoInterruptLeftOver,
+		MaxBidsPerBuilder:     *miner.worker.config.Mev.MaxBidsPerBuilder,
 		GasCeil:               miner.worker.config.GasCeil,
 		GasPrice:              miner.worker.config.GasPrice,
 		BuilderFeeCeil:        builderFeeCeil,
