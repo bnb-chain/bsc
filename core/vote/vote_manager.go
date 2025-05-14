d173feee3d55a3f015aa5aa46c9d271cbd56496d
buddho <galaxystroller@gmail.com>
2025-05-14 17:17:14 +0800

    core/vote: change waiting blocks for voting since start mining (#3088)

--- core/vote/vote_manager.go
+++ core/vote/vote_manager.go
@@ -17,7 +17,12 @@ import (
 	"github.com/ethereum/go-ethereum/metrics"
 )
 
-const blocksNumberSinceMining = 5 // the number of blocks need to wait before voting, counting from the validator begin to mine
+// Many validators maintain backup machines.
+// When switching from a primary node to a backup (e.g., due to failure),
+// the new node may cast votes for the same block height that the previous node already voted on.
+// To avoid double-voting issues, the node should wait for a few blocks
+// before participating in voting after it starts mining.
+const blocksNumberSinceMining = 20
 
 var diffInTurn = big.NewInt(2) // Block difficulty for in-turn signatures
 var votesManagerCounter = metrics.NewRegisteredCounter("votesManager/local", nil)
