08f0bebbb2ee3df07f903430db6853cea652697d
galaio <12880651+galaio@users.noreply.github.com>
2025-05-19 16:02:37 +0800

    p2p: fix forwarding block of proxied validator issue in EVN; (#3101)

--- p2p/config.go
+++ p2p/config.go
@@ -20,6 +20,7 @@ import (
 	"crypto/ecdsa"
 	"fmt"
 
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/mclock"
 	"github.com/ethereum/go-ethereum/log"
 	"github.com/ethereum/go-ethereum/p2p/enode"
@@ -91,9 +92,9 @@ type Config struct {
 	// the list is another choice for non-validator nodes to get block quickly
 	EVNNodeIdsWhitelist []enode.ID `toml:",omitempty"`
 
-	// ProxyedValidatorNodeIDs is a list of validator NodeIDs that the local node proxies,
+	// ProxyedValidatorAddresses is a list of validator addresses that the local node proxies,
 	// it usually used for sentry nodes
-	ProxyedValidatorNodeIDs []enode.ID `toml:",omitempty"`
+	ProxyedValidatorAddresses []common.Address `toml:",omitempty"`
 
 	// Connectivity can be restricted to certain IP networks.
 	// If this option is set to a non-nil value, only hosts which match one of the
