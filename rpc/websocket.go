f86e7b589bd904b514a3eae1a058cf7c20a6d5c6
tongjicoder <tongjicoder@icloud.com>
2025-05-28 11:34:15 +0800

    refactor: use maps.copy for cleaner map handling (#3123)

--- rpc/websocket.go
+++ rpc/websocket.go
@@ -20,6 +20,7 @@ import (
 	"context"
 	"encoding/base64"
 	"fmt"
+	"maps"
 	"net/http"
 	"net/url"
 	"os"
@@ -240,9 +241,7 @@ func newClientTransportWS(endpoint string, cfg *clientConfig) (reconnectFunc, er
 	if err != nil {
 		return nil, err
 	}
-	for key, values := range cfg.httpHeaders {
-		header[key] = values
-	}
+	maps.Copy(header, cfg.httpHeaders)
 
 	connect := func(ctx context.Context) (ServerCodec, error) {
 		header := header.Clone()
