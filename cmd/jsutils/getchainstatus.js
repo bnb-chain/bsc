e89a475ee451f848ee90a5a2aa2b9c739873682f
buddh0 <galaxystroller@gmail.com>
2025-06-12 16:27:40 +0800

    cmd/jsutils: add more builders for chapel

--- cmd/jsutils/getchainstatus.js
+++ cmd/jsutils/getchainstatus.js
@@ -247,7 +247,7 @@ const builderMap = new Map([
     ["0xb49f86586a840AB9920D2f340a85586E50FD30a2", "inblock eu"],
     ["0x0F6D8b72F3687de6f2824903a83B3ba13c0e88A0", "inblock us"],
     //      nodereal
-    ["0x79102dB16781ddDfF63F301C9Be557Fd1Dd48fA0", "nodereal ap x"],
+    ["0x79102dB16781ddDfF63F301C9Be557Fd1Dd48fA0", "nodereal ap"],
     ["0xd0d56b330a0dea077208b96910ce452fd77e1b6f", "nodereal eu"],
     ["0x4f24ce4cd03a6503de97cf139af2c26347930b99", "nodereal us"],
     //      xzbuilder
@@ -255,8 +255,12 @@ const builderMap = new Map([
 
     // Chapel
     ["0x627fE6AFA2E84e461CB7AE7C2c46e8adf9a954a2", "txboost"],
-    ["0x5EC60f73f938e36400ec3CC3Ff4d7a7703F7c005", "nodereal ap y"],
-    // ["0x4827b423D03a349b7519Dda537e9A28d31ecBB48", "puissant y"],
+    ["0x5EC60f73f938e36400ec3CC3Ff4d7a7703F7c005", "nodereal ap"],
+    ["0x6C98EB21139F6E12db5b78a4AeD4d8eBA147FB7b", "nodereal eu"],
+    // ["0x79102dB16781ddDfF63F301C9Be557Fd1Dd48fA0", "nodereal us"],
+    ["0x4827b423D03a349b7519Dda537e9A28d31ecBB48", "club48 ap"],
+    ["0x48B2665E5E9a343409199D70F7495c8aB660BB48", "club48 eu"],
+    ["0x48B4bBEbF0655557A461e91B8905b85864B8BB48", "club48 us"],
     ["0x0eAbBdE133fbF3c5eB2BEE6F7c8210deEAA0f7db", "blockrazor ap"],
     ["0x95c8436143c82Ea4d3529A3ed8DDa9998F6daC5F", "blockrazor eu"],
     ["0xb71Ba9e570ee20E983De1d5aE01baf5dCB4e4299", "blockrazor us"],
@@ -754,7 +758,7 @@ async function getMevStatus() {
     let counts = {
         local: 0,
         blockrazor: 0,
-        puissant: 0,
+        club48: 0,
         blockroute: 0,
         jetbldr: 0,
         txboost: 0,
