2853fdce20de4425c91764567559802127c766a0
buddh0 <galaxystroller@gmail.com>
2025-06-17 10:00:02 +0800

    params: add FermiTime

--- cmd/geth/config.go
+++ cmd/geth/config.go
@@ -216,6 +216,10 @@ func makeFullNode(ctx *cli.Context) (*node.Node, ethapi.Backend) {
 		v := ctx.Uint64(utils.OverrideMaxwell.Name)
 		cfg.Eth.OverrideMaxwell = &v
 	}
+	if ctx.IsSet(utils.OverrideFermi.Name) {
+		v := ctx.Uint64(utils.OverrideFermi.Name)
+		cfg.Eth.OverrideFermi = &v
+	}
 	if ctx.IsSet(utils.OverrideVerkle.Name) {
 		v := ctx.Uint64(utils.OverrideVerkle.Name)
 		cfg.Eth.OverrideVerkle = &v
