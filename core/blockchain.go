4252ecbcd8d33b20045b93a7fa8739c79256b795
VM <arimas@foxmail.com>
2025-06-19 20:38:06 +0800

    feat: add incremental path flag

--- core/blockchain.go
+++ core/blockchain.go
@@ -1844,9 +1844,9 @@ func (bc *BlockChain) writeBlockWithState(block *types.Block, receipts []*types.
 		wg.Done()
 	}()
 
-	log.Info("statedb commit", "block number", block.NumberU64(), "current", bc.CurrentBlock().Number.Uint64(),
-		"snap", bc.CurrentSnapBlock().Number.Uint64(), "safe", bc.CurrentSafeBlock().Number.Uint64(),
-		"final", bc.CurrentFinalBlock().Number.Uint64(), "head block", bc.CurrentHeader().Number.Uint64())
+	// log.Info("statedb commit", "block number", block.NumberU64(), "current", bc.CurrentBlock().Number.Uint64(),
+	// 	"snap", bc.CurrentSnapBlock().Number.Uint64(), "safe", bc.CurrentSafeBlock().Number.Uint64(),
+	// 	"final", bc.CurrentFinalBlock().Number.Uint64(), "head block", bc.CurrentHeader().Number.Uint64())
 	// Commit all cached state changes into underlying memory database.
 	root, diffLayer, err := statedb.Commit(block.NumberU64(), bc.chainConfig.IsEIP158(block.Number()), bc.chainConfig.IsCancun(block.Number(), block.Time()))
 	if err != nil {
