47d3146cd276663afdcd8bbf4bb90e5ddbe6bf6e
annielz <lmlmzhen@sina.com>
2025-06-27 09:42:14 +0800

    feat: remove ratio log

--- core/vm/evm.go
+++ core/vm/evm.go
@@ -30,7 +30,6 @@ import (
 	"github.com/ethereum/go-ethereum/core/tracing"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
-	"github.com/ethereum/go-ethereum/log"
 	"github.com/ethereum/go-ethereum/params"
 )
 
@@ -489,7 +488,7 @@ func tryGetOptimizedCode(evm *EVM, codeHash common.Hash, rawCode []byte) (bool, 
 	} else {
 		compiler.GenOrLoadOptimizedCode(codeHash, rawCode)
 	}
-	log.Error("tryGetOptimizedCode optimized calc", "optimized", optimized) // method to calc optimized rate, comment when not used
+	//log.Error("tryGetOptimizedCode optimized calc", "optimized", optimized) // method to calc optimized rate, comment when not used
 	return optimized, code
 }
 
