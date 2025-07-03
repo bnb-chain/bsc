ebedc3744a70b62a7c0666f28111c2df49e6384a
buddh0 <galaxystroller@gmail.com>
2025-07-03 10:58:59 +0800

    consensus/parlia: warn fast node to update to latest hard fork version

--- core/state/statedb_hooked.go
+++ core/state/statedb_hooked.go
@@ -269,6 +269,10 @@ func (s *hookedStateDB) SelfDestruct6780(address common.Address) (uint256.Int, b
 	return prev, changed
 }
 
+func (s *hookedStateDB) NoTrie() bool {
+	return s.inner.NoTrie()
+}
+
 func (s *hookedStateDB) AddLog(log *types.Log) {
 	// The inner will modify the log (add fields), so invoke that first
 	s.inner.AddLog(log)
