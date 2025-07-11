e3dcb40ce647ff94cdbb82edbe7252886e53dd3f
annielz <lmlmzhen@sina.com>
2025-07-11 16:40:32 +0800

    feat:comment cur+1 leave 5

--- core/opcodeCompiler/compiler/opCodeProcessor.go
+++ core/opcodeCompiler/compiler/opCodeProcessor.go
@@ -475,19 +475,19 @@ func doCodeFusion(code []byte) ([]byte, error) {
 			//	skipToNext = true
 			//}
 
-			if code0 == SWAP2 && code1 == SWAP1 {
-				op := Swap2Swap1
-				fusedCode[cur] = byte(op)
-				fusedCode[cur+1] = byte(Nop)
-				skipToNext = true
-			}
-			//
-			//if code0 == SWAP2 && code1 == POP {
-			//	op := Swap2Pop
+			//if code0 == SWAP2 && code1 == SWAP1 {
+			//	op := Swap2Swap1
 			//	fusedCode[cur] = byte(op)
 			//	fusedCode[cur+1] = byte(Nop)
 			//	skipToNext = true
 			//}
+			//
+			if code0 == SWAP2 && code1 == POP {
+				op := Swap2Pop
+				fusedCode[cur] = byte(op)
+				fusedCode[cur+1] = byte(Nop)
+				skipToNext = true
+			}
 			//
 			//if code0 == DUP2 && code1 == LT {
 			//	op := Dup2LT
