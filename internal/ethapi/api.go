12c691e3ea27ba327e59c6c5473504aef01c8548
FT <140458077+zeevick10@users.noreply.github.com>
2025-07-15 00:08:06 +0200

    all: fix outdated ethereum wiki json-rpc json-rpc doc links (#32209)
    Replace outdated wiki reference with ethereum.org
    documentation links

--- internal/ethapi/api.go
+++ internal/ethapi/api.go
@@ -2062,7 +2062,7 @@ func (api *TransactionAPI) SendRawTransactionConditional(ctx context.Context, in
 //
 // The account associated with addr must be unlocked.
 //
-// https://github.com/ethereum/wiki/wiki/JSON-RPC#eth_sign
+// https://ethereum.org/en/developers/docs/apis/json-rpc/#eth_sign
 func (api *TransactionAPI) Sign(addr common.Address, data hexutil.Bytes) (hexutil.Bytes, error) {
 	// Look up the wallet containing the requested signer
 	account := accounts.Account{Address: addr}
