0c0cd2c63510bc535c7b5454f4c746e80af888ae
Sunny <sunny2022.za@gmail.com>
2025-07-18 15:02:17 +0800

    enlarge codecache capacity and reduce opcode processor number

--- core/opcodeCompiler/compiler/opCodeProcessor.go
+++ core/opcodeCompiler/compiler/opCodeProcessor.go
@@ -44,7 +44,7 @@ type optimizeTask struct {
 
 func init() {
 	taskChannel = make(chan optimizeTask, taskChannelSize)
-	taskNumber := runtime.NumCPU() * 3 / 8
+	taskNumber := runtime.NumCPU() * 1 / 4
 	if taskNumber < 1 {
 		taskNumber = 1
 	}
