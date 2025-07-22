3cf42d62c23e9aea3ff0ab4cff546d3cd55baaad
VM <arimas@foxmail.com>
2025-07-22 21:04:14 +0800

    fix: add debug logs

--- triedb/pathdb/buffer.go
+++ triedb/pathdb/buffer.go
@@ -140,7 +140,7 @@ func (b *buffer) flush(db ethdb.KeyValueStore, freezer ethdb.AncientWriter, node
 	// Ensure the target state id is aligned with the internal counter.
 	head := rawdb.ReadPersistentStateID(db)
 	if head+b.layers != id {
-		log.Error("buffer layers (%d) cannot be applied on top of persisted state id (%d) to reach requested state id (%d)", b.layers, head, id)
+		log.Error("buffer layers cannot be applied on top of persisted state id to reach requested state id", "layers", b.layers, "head", head, "id", id)
 		return fmt.Errorf("buffer layers (%d) cannot be applied on top of persisted state id (%d) to reach requested state id (%d)", b.layers, head, id)
 	}
 	// Terminate the state snapshot generation if it's active
