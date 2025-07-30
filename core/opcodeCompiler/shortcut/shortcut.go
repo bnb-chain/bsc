02e8553c862883024497e2df8114c00e729129a9
rekyyang <511965710@qq.com>
2025-07-30 19:28:47 +0800

    opt

--- core/opcodeCompiler/shortcut/shortcut.go
+++ core/opcodeCompiler/shortcut/shortcut.go
@@ -4,6 +4,7 @@ import (
 	"github.com/holiman/uint256"
 
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/opcodeCompiler/shortcut/impl"
 	"github.com/ethereum/go-ethereum/log"
 )
 
@@ -22,6 +23,15 @@ func RegisterShortcut(addr common.Address, s Shortcut) {
 func GetShortcut(addr common.Address) Shortcut {
 	//log.Info("Getting shortcut", "addr", addr.String())
 	return shortcutPcRegisters[addr]
+}
+
+func GetShortcutV2(addr common.Address) Shortcut {
+	switch string(addr.Bytes()) {
+	case string([]byte{0x55, 0xD3, 0x98, 0x32, 0x6F, 0x99, 0x05, 0x9F, 0xF7, 0x75, 0x48, 0x52, 0x46, 0x99, 0x90, 0x27, 0xB3, 0x19, 0x79, 0x55}):
+		return &impl.ShortcutImpl55D398326F99059FF775485246999027B3197955{}
+	default:
+		return nil
+	}
 }
 
 type Shortcut interface {
