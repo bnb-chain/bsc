9bfa7dd6eda69091dc3b932c2c30319c7b47e883
rekyyang <511965710@qq.com>
2025-07-31 22:58:51 +0800

    fix

--- cmd/test/test.go
+++ cmd/test/test.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/crypto"
 )
 
 const (
@@ -9,25 +13,25 @@ const (
 )
 
 func main() {
-	stk := make([]byte, 0)
-	stk2 := make([]byte, 0)
+	//query := hexutil.MustDecode("0x000000000000000000000000765388e911Ab4bcBB6da83793dF3F8EE4783a9Ef")
+	query := hexutil.MustDecode("0x000000000000000000000000765388e911ab4bcbb6da83793df3f8ee4783a9ef")
+	// 0x40a42e9f7cb0da6f665cee9a364aeda2965a9b595de818a7a3136549fd9417d8
 
-	fmt.Println(cap(stk))
+	query = append(query,
+		0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+		0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+		0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0,
+		0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x1,
+	)
 
-	for i := 0; i < 100; i++ {
-		stk = append(stk, byte(i))
-		//stk2 = append(stk2, byte(i))
-		//stk2 = append(stk2, byte(i))
-		//stk2 = append(stk2, byte(i))
-	}
+	hasherBuf := common.Hash{}
 
-	fmt.Println(cap(stk))
+	hasher := crypto.NewKeccakState()
+	hasher.Write(query[:])
+	hasher.Read(hasherBuf[:])
+	fmt.Printf("slot %s\n", hasherBuf.String())
 
-	stk = stk2
+	buf2 := crypto.Keccak256(query)
 
-	fmt.Println(cap(stk))
-
-	stk = stk[:0]
-
-	fmt.Println(cap(stk))
+	fmt.Printf("slot %s\n", hexutil.Encode(buf2))
 }
