2c98d873d26803e9d99de40fd7d82d22cb27c95d
zzzckck <152148891+zzzckck@users.noreply.github.com>
2025-08-04 10:46:13 +0800

    jsutil: fix a getSlashCount issue

--- cmd/jsutils/getchainstatus.js
+++ cmd/jsutils/getchainstatus.js
@@ -32,7 +32,7 @@ function printUsage() {
     console.log("\nOptions:");
     console.log("  --rpc        specify the url of RPC endpoint");
     console.log("               mainnet: https://bsc-mainnet.nodereal.io/v1/cc07638d01a64904a662599433827378");
-    console.log("               testnet: https://bsc-testnet-dataseed.bnbchain.org");
+    console.log("               testnet: https://bsc-testnet.nodereal.io/v1/cc07638d01a64904a662599433827378");
     console.log("  --startNum   the start block number");
     console.log("  --endNum     the end block number");
     console.log("  --miner      the miner address");
@@ -206,6 +206,7 @@ const validatorMap = new Map([
     ['0x28D70c3756d4939DCBdEB3f0fFF5B4B36E6e327F', [ 'OmegaV'   , '0x1875ADAe6aaAED32A87DbcD2eEbf076EA86caA35', '0xb8db8ca018a14d843b560d8fabde98c3d47ad6ceea9d510d3357e2ff687acf19f9431aa180b17222a3a9328238297a1a']],
     ['0x6a5470a3B7959ab064d6815e349eD4aE2dE5210d', ['Skynet10k' , '0xDD1fD7C74BaCCA08e1b88a24199F19aB1b1b9cE4', '0x81f13afdbd6976d9784a05619405df430314e2707050b32f29ae683b9ef89d285d1a227df3e31ac147016c4c7533be70']],
     ['0xce6cCa0DE7b3EB3fd0CcE4bc38cceF473166e4f4', ['Infinity'  , '0xc8A6Bfe0834FB99340a0Df253d79B7CaE25053b8', '0xa40f553889e9de6b4fe8005a06d7335fa061ae51ef5ba2b0c4ea477fcaa8f6de1650e318cf59824462b1831a725488da']],
+    ['0xa7deE0bCAEb78849Ec4aD4e2f48688D2e9f2315B', ['KrakV'     , '0x6563AA29C30d9f80968c2fb7DFFed092a03FBdeD', '0x848ffc9a3fac00d9fbaebcb63f2b7c0a4747d9ffecd4b484073ad03d91584cb51af29870c1c8421b757f4f6fae813288']],
 ]);
 
 const builderMap = new Map([
@@ -407,8 +408,8 @@ async function getSlashCountAtHeight(num) {
     }
     let slashScale = await validatorSet.maintainSlashScale({ blockTag: blockNum });
     let maxElected = await stakeHub.maxElectedValidators({ blockTag: blockNum });
-    const maintainThreshold = BigInt(50); // governable, hardcode to avoid one RPC call
-    const felonyThreshold = BigInt(150); // governable, hardcode to avoid one RPC call
+    const maintainThreshold = BigInt(150); // governable, hardcode to avoid one RPC call
+    const felonyThreshold = BigInt(600); // governable, hardcode to avoid one RPC call
 
     let block = await provider.getBlock(blockNum);
     console.log("At block", blockNum, "time", block.date);
@@ -452,8 +453,8 @@ async function getSlashCount() {
         stepNum = 1;
     }
     for (let i = 0; i < stepNum; i++) {
-        blockNum = blockNum - stepLength;
         await getSlashCountAtHeight(blockNum);
+        blockNum = blockNum - stepLength;
     }
 }
 // 5.cmd: "getPerformanceData", usage:
