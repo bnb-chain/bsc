4235fca3ae2154ea5927e31651700483705d48db
rekyyang <511965710@qq.com>
2025-08-05 21:32:03 +0800

    add logs

--- core/vm/interpreter.go
+++ core/vm/interpreter.go
@@ -378,6 +378,10 @@ func (in *EVMInterpreter) Run(contract *Contract, input []byte, readOnly bool) (
 			contract.Gas -= cost
 		}
 
+		if op == CREATE || op == CREATE2 || op == CALL || op == STATICCALL || op == DELEGATECALL || op == CALLCODE {
+			log.Error("debug op cost", "op", op.String(), "cost", cost)
+		}
+
 		// All ops with a dynamic memory usage also has a dynamic gas cost.
 		var memorySize uint64
 		if operation.dynamicGas != nil {
@@ -410,6 +414,10 @@ func (in *EVMInterpreter) Run(contract *Contract, input []byte, readOnly bool) (
 				return nil, ErrOutOfGas
 			} else {
 				contract.Gas -= dynamicCost
+			}
+
+			if op == CREATE || op == CREATE2 || op == CALL || op == STATICCALL || op == DELEGATECALL || op == CALLCODE {
+				log.Error("debug op cost", "op", op.String(), "cost dynamic", dynamicCost)
 			}
 		}
 
