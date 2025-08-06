d36fd060b6a5c6509a7bd00033904f75a6d61b0a
VM <arimas@foxmail.com>
2025-08-06 16:11:39 +0800

    fix: solve ci errors

--- triedb/pathdb/states.go
+++ triedb/pathdb/states.go
@@ -22,10 +22,8 @@ import (
 	"slices"
 	"sync"
 
-	"github.com/VictoriaMetrics/fastcache"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/rawdb"
-	"github.com/ethereum/go-ethereum/ethdb"
 	"github.com/ethereum/go-ethereum/log"
 	"github.com/ethereum/go-ethereum/metrics"
 	"github.com/ethereum/go-ethereum/rlp"
