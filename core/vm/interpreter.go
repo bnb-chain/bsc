d54149e5a1890f33f08b6939c88af13dbb2cbd60
annielz <lmlmzhen@sina.com>
2025-08-27 16:29:36 +0800

    feat: tune logs to debug block 50897382

--- core/vm/interpreter.go
+++ core/vm/interpreter.go
@@ -324,7 +324,7 @@ func (in *EVMInterpreter) Run(contract *Contract, input []byte, readOnly bool) (
 		cost = operation.constantGas // For tracing todo: move into if
 		totalCost += cost
 		// New debug line: print opcode, pc and its static gas each step
-		if in.evm.Context.BlockNumber.Uint64() == 50897372 && in.evm.StateDB.TxIndex() == 70 {
+		if in.evm.Context.BlockNumber.Uint64() == 50897402 && in.evm.StateDB.TxIndex() == 70 {
 			log.Error("[OP STATIC]", "pc", pc, "opcode", op.String(), "staticGas", cost, "totalCost", totalCost, "contract.Gas", contract.Gas, "stackLen", stack.len())
 		}
 		// 暂不打印，改为在动态 gas 处理后统一输出（保证包含 dynamic 与 chunk 等影响后的净消耗）
