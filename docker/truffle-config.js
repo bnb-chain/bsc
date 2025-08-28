4c4c72f44e931613186edeae569606011758610b
allformless <213398294+allformless@users.noreply.github.com>
2025-08-28 14:52:12 +0800

    docker: use official solidity compiler binary instead of js version

--- docker/truffle-config.js
+++ docker/truffle-config.js
@@ -60,7 +60,7 @@ module.exports = {
     compilers: {
       solc: {
         version: "0.6.4",    // Fetch exact version from solc-bin (default: truffle's version)
-        docker: false,        // Use "0.5.1" you've installed locally with docker (default: false)
+        docker: true,        // Use "0.6.4" you've installed locally with docker (default: false)
         settings: {          // See the solidity docs for advice about optimization and evmVersion
          optimizer: {
            enabled: true,
