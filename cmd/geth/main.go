9830ceac1560165d01b62edafc04d2fdbf6bcf99
cbh876 <3930922419@qq.com>
2025-08-29 15:49:02 +0800

    feat: SuperInstruction implementation in EVM

--- cmd/geth/main.go
+++ cmd/geth/main.go
@@ -182,6 +182,7 @@ var (
 		utils.LogDebugFlag,
 		utils.LogBacktraceAtFlag,
 		utils.BlobExtraReserveFlag,
+		utils.VMOpcodeOptimizeFlag,
 		// utils.BeaconApiFlag,
 		// utils.BeaconApiHeaderFlag,
 		// utils.BeaconThresholdFlag,
