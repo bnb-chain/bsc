7a03cc085459e0be37759efe1471a09eb5a4d56a
zzzckck <152148891+zzzckck@users.noreply.github.com>
2025-09-30 19:09:31 +0800

    release: v1.6.101 for scalableDB feature release

--- version/version.go
+++ version/version.go
@@ -17,8 +17,8 @@
 package version
 
 const (
-	Major = 1  // Major version component of the current release
-	Minor = 6  // Minor version component of the current release
-	Patch = 3  // Patch version component of the current release
-	Meta  = "" // Version metadata to append to the version string
+	Major = 1   // Major version component of the current release
+	Minor = 6   // Minor version component of the current release
+	Patch = 101 // Patch version component of the current release
+	Meta  = ""  // Version metadata to append to the version string
 )
