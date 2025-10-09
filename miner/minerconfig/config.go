5a2a89c0e3c0b63bba198371ea09cb7c61a52682
allformless <213398294+allformless@users.noreply.github.com>
2025-10-09 11:05:33 +0800

    miner/minerconfig: update default GasCeil to 100M

--- miner/minerconfig/config.go
+++ miner/minerconfig/config.go
@@ -69,7 +69,7 @@ type Config struct {
 
 // DefaultConfig contains default settings for miner.
 var DefaultConfig = Config{
-	GasCeil:  75000000,
+	GasCeil:  100000000,
 	GasPrice: big.NewInt(params.GWei),
 
 	// The default recommit time is chosen as two seconds since
