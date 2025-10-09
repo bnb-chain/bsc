1ddd055a869f1173c0e68792864a5b76fc8e3380
zzzckck <152148891+zzzckck@users.noreply.github.com>
2025-10-09 11:42:47 +0800

    release: prepare for release v1.6.2 (#3386)

--- version/version.go
+++ version/version.go
@@ -17,8 +17,8 @@
 package version
 
 const (
-	Major = 1     // Major version component of the current release
-	Minor = 6     // Minor version component of the current release
-	Patch = 100   // Patch version component of the current release
-	Meta  = "BAL" // Version metadata to append to the version string
+	Major = 1  // Major version component of the current release
+	Minor = 6  // Minor version component of the current release
+	Patch = 2  // Patch version component of the current release
+	Meta  = "" // Version metadata to append to the version string
 )
