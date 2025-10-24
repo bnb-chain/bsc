097a2319f85e2683da72cd6bece685619d622194
fynnss <zcheng1004@gmail.com>
2025-10-24 11:27:58 +0800

    fix

--- core/rawdb/accessors_chain.go
+++ core/rawdb/accessors_chain.go
@@ -960,47 +960,6 @@ func DeleteBlobSidecars(db ethdb.KeyValueWriter, hash common.Hash, number uint64
 	}
 }
 
-// ReadBALRLP retrieves all the block access list belonging to a block in RLP encoding.
-func ReadBALRLP(db ethdb.Reader, hash common.Hash, number uint64) rlp.RawValue {
-	// BAL is only in kv DB, will not be put into ancient DB
-	data, _ := db.Get(blockBALKey(number, hash))
-	return data
-}
-
-// ReadBAL retrieves the block access list belonging to a block.
-func ReadBAL(db ethdb.Reader, hash common.Hash, number uint64) *types.BlockAccessListEncode {
-	data := ReadBALRLP(db, hash, number)
-	if len(data) == 0 {
-		return nil
-	}
-	var ret types.BlockAccessListEncode
-	if err := rlp.DecodeBytes(data, &ret); err != nil {
-		log.Error("Invalid BAL RLP", "hash", hash, "err", err)
-		return nil
-	}
-	return &ret
-}
-
-func WriteBAL(db ethdb.KeyValueWriter, hash common.Hash, number uint64, bal *types.BlockAccessListEncode) {
-	if bal == nil {
-		return
-	}
-	data, err := rlp.EncodeToBytes(bal)
-	if err != nil {
-		log.Crit("Failed to encode block BAL", "err", err)
-	}
-
-	if err := db.Put(blockBALKey(number, hash), data); err != nil {
-		log.Crit("Failed to store block BAL", "err", err)
-	}
-}
-
-func DeleteBAL(db ethdb.KeyValueWriter, hash common.Hash, number uint64) {
-	if err := db.Delete(blockBALKey(number, hash)); err != nil {
-		log.Crit("Failed to delete block BAL", "err", err)
-	}
-}
-
 // WriteAncientHeaderChain writes the supplied headers along with nil block
 // bodies and receipts into the ancient store. It's supposed to be used for
 // storing chain segment before the chain cutoff.
