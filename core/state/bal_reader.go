de77852b55a61f705af1d78e7e065475ae132c2e
fynnss <zcheng1004@gmail.com>
2025-10-24 14:13:51 +0800

    fix bal ignore

--- core/state/bal_reader.go
+++ core/state/bal_reader.go
@@ -138,6 +138,10 @@ func (s *BALReader) initMutatedObjFromDiff(db *StateDB, addr common.Address, a *
 
 var IgnoredBALAddresses map[common.Address]struct{} = map[common.Address]struct{}{
 	params.SystemAddress: {},
+	// Nano block list
+	common.HexToAddress("0x489A8756C18C0b8B24EC2a2b9FF3D4d447F79BEc"): {},
+	common.HexToAddress("0xFd6042Df3D74ce9959922FeC559d7995F3933c55"): {},
+	common.HexToAddress("0xdb789Eb5BDb4E559beD199B8b82dED94e1d056C9"): {},
 }
 
 // BALReader provides methods for reading account state from a block access
