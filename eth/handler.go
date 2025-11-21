537eb74eddb8221e21b7c9f6bf201c4acf30fa69
siwen <494631748@qq.com>
2025-11-21 20:00:01 +0800

    error

--- eth/handler.go
+++ eth/handler.go
@@ -667,12 +667,12 @@ func (h *handler) peerLatencyCollectionLoop() {
 			// 获取所有peer
 			peers := h.peers.list()
 			for _, peer := range peers {
-				// 获取底层p2p peer的延迟信息 (毫秒)
+				// 通过 Info() 方法获取延迟信息 (毫秒)
 				if peer.Peer != nil && peer.Peer.Peer != nil {
-					latency := peer.Peer.Peer.Latency()
-					if latency > 0 {
+					info := peer.Peer.Peer.Info()
+					if info != nil && info.Latency > 0 {
 						// 记录延迟到白名单
-						h.peerWhitelist.recordLatency(peer.ID(), latency)
+						h.peerWhitelist.recordLatency(peer.ID(), info.Latency)
 					}
 				}
 			}
