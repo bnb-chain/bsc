be47203a83dc2e6253a95a250b301c6705538949
siwen <494631748@qq.com>
2026-01-16 01:02:19 +0800

    去掉日志

--- core/rawdb/chain_iterator.go
+++ core/rawdb/chain_iterator.go
@@ -150,7 +150,7 @@ func iterateTransactions(db ethdb.Database, from uint64, to uint64, reverse bool
 		for data := range rlpCh {
 			var body types.Body
 			if err := rlp.DecodeBytes(data.rlp, &body); err != nil {
-				log.Warn("Failed to decode block body", "block", data.number, "error", err)
+				// log.Warn("Failed to decode block body", "block", data.number, "error", err)
 				return
 			}
 			var hashes []common.Hash
